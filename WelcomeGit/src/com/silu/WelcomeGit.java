4780ae08399fdc906a9837bdc7ae0331bce7ff51
silu <java.prasanna91@gmail.com>
2017-04-30 20:40:37 +0530

    Again modify line no 8.

--- WelcomeGit/src/com/silu/WelcomeGit.java
+++ WelcomeGit/src/com/silu/WelcomeGit.java
@@ -5,6 +5,6 @@ public class WelcomeGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("welcome github");
-		System.out.println("welcome git 1(dev1)");
+		System.out.println("welcome git 1(dev1..........)");
 	}
 }
